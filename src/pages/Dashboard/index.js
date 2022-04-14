39aaeaed99f77efeac168a1d3b65b79416052301
Bruno <bru_blink@hotmail.com>
2022-04-14 17:43:21 -0300

    refatorado para o limite do grafico ser baseado no total dos ativos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -25,86 +25,117 @@ import {
 
 function Dashboard() {
   const { user } = useContext(AuthContext);
-    
+
   const { debitos, getDebitos } = useContext(DebitosContext);
   const { investimento, getInvestimentos } = useContext(InvestimentosContext);
 
   const [dataCategoriasAgrupadas, setDataCategoriasAgrupadas] = useState([]);
   const [dataContasPagoTotal, setContasPagoTotal] = useState([]);
   const [dataInvestimento, setDataInvestimento] = useState([]);
+  const [dataMaiorInvestimento, setDataMaiorInvestimento] = useState([]);
+  const [dataMaiorDebito, setDataMaiorDebito] = useState([]);
 
-  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF0000", "#800000", "#808000", 
-                  "#00FF00", "#008000" , "#00FFFF", "#008080", "#0000FF", "#000080", "#FF00FF", "#800080"]
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF0000", "#800000", "#808000",
+    "#00FF00", "#008000", "#00FFFF", "#008080", "#0000FF", "#000080", "#FF00FF", "#800080"]
 
+  // user.id
   useEffect(() => {
-    getDebitos(user.uid);
-    getInvestimentos(user.uid);
+    getDebitos("qUgqP2CTNlVBeZQxFqYnI8gYRMu2");
+    getInvestimentos("qUgqP2CTNlVBeZQxFqYnI8gYRMu2");
   }, [])
 
   useEffect(() => {
-    CalculosDebito();    
+    CalculosDebito();
   }, [debitos])
 
   useEffect(() => {
     CalculaInvestimento();
   }, [investimento])
 
-  function CalculaInvestimento(){
-
+  function CalculaInvestimento() {
     let investimentosLocal = [];
-    let porcentagem = 0;
+    let valorTotalInvestimentos = 0;
 
-    investimento.forEach((inv) => {
-      porcentagem = (1 / investimento.length) * 100;
-      investimentosLocal.push({ ativo: inv.ativo, porcentagem: parseFloat(porcentagem.toFixed(2)) })
+    investimento.forEach((investimento) => {
+      valorTotalInvestimentos += parseFloat(investimento.valorinvestido);
+      CalculoCategoriasInvestimento(investimentosLocal, investimento);
     });
-    setDataInvestimento(investimentosLocal);
 
+    investimentosLocal.forEach(invLocal => {
+      invLocal.porcentagem = parseFloat(((parseFloat(invLocal.valor) / valorTotalInvestimentos) * 100).toFixed(2));
+    });
+
+
+    setDataMaiorInvestimento(ObtemMaiorValor(investimentosLocal));
+    setDataInvestimento(investimentosLocal);
   }
 
-  function CalculosDebito(){
+  function CalculosDebito() {
     let pagamentos = {
       pagar: 0,
       pago: 0
     }
     const categoriasAgrupadas = [];
     const contasPagoTotal = [];
-
-
+    let valorTotalDebitos = 0;
     debitos.forEach(debito => {
+      valorTotalDebitos += parseFloat(debito.valor)
       CalculoPagamentos(pagamentos, debito);
       CalculoCategorias(categoriasAgrupadas, debito);
     });
 
     categoriasAgrupadas.forEach(categoria => {
-      categoria.porcentagem = parseFloat(((1 / categoriasAgrupadas.length) * 100).toFixed(2));
+      categoria.porcentagem = parseFloat(((parseFloat(categoria.valor) / valorTotalDebitos) * 100).toFixed(2));
     });
-
 
     contasPagoTotal.push({ nome: "Pagar", valor: parseFloat(pagamentos.pagar.toFixed(2)) });
     contasPagoTotal.push({ nome: "Pago", valor: parseFloat(pagamentos.pago.toFixed(2)) });
 
+
+    setDataMaiorDebito(ObtemMaiorValor(categoriasAgrupadas));
     setDataCategoriasAgrupadas(categoriasAgrupadas);
     setContasPagoTotal(contasPagoTotal);
   }
 
-  function CalculoPagamentos(pagamentos, debito){
+  function CalculoPagamentos(pagamentos, debito) {
     pagamentos.pagar = parseFloat(debito.valor) + parseFloat(pagamentos.pagar)
     if (debito.situacao === "Pago") {
       pagamentos.pago = parseFloat(debito.valor) + parseFloat(pagamentos.pago);
     }
   }
 
+  function CalculoCategoriasInvestimento(investimentosLocal, investimentoCTX) {
+    let indexInvestimento = investimentosLocal.findIndex(investimento => investimento.ativo === investimentoCTX.invest);
+    if (indexInvestimento != -1) {
+      let valorArray = parseFloat(investimentosLocal[indexInvestimento].valor);
+      let valorInvestimento = parseFloat(investimentoCTX.valorinvestido);
+      investimentosLocal[indexInvestimento].valor = (valorArray + valorInvestimento).toFixed(2);
+    }
+    else {
+      investimentosLocal.push({ ativo: investimentoCTX.invest, porcentagem: 0, valor: investimentoCTX.valorinvestido });
+    }
+  }
+
   function CalculoCategorias(categoriasAgrupadas, debito) {
     let indexCategoria = categoriasAgrupadas.findIndex(categoria => categoria.nome === debito.categoria);
-    if(indexCategoria != -1){
+    if (indexCategoria != -1) {
       let valorArray = parseFloat(categoriasAgrupadas[indexCategoria].valor);
       let valorDebito = parseFloat(debito.valor);
       categoriasAgrupadas[indexCategoria].valor = (valorArray + valorDebito).toFixed(2);
     }
-    else{
+    else {
       categoriasAgrupadas.push({ nome: debito.categoria, valor: parseFloat(debito.valor).toFixed(2), porcentagem: 0 })
     }
+  }
+  
+  
+  function ObtemMaiorValor(array) {
+    const valores = array.map(object => {
+      return object.valor;
+    });
+
+    const max = Math.max(...valores);
+    return max;
   }
 
   return (
@@ -114,14 +145,28 @@ function Dashboard() {
         <Title nome="DashBoard">
           <FiHome size={25} />
         </Title>
-        <div className="container-dash">   
+        <div className="container-dash">
+          <div>
+            <div className="text-graph">
+              Contas Pagas VS A Pagar
+            </div>
+            <PieChart width={500} height={300}>
+              <Legend />
+              <Tooltip />
+              <Pie data={dataContasPagoTotal} label dataKey="valor" nameKey="nome" cx="50%" cy="50%" outerRadius={100} fill="#8884d8">
+                {dataContasPagoTotal.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+            </PieChart>
+          </div>
           <div className="charts">
             <div>
               <div className="text-graph">
                 Valor Por Categoria
               </div>
               <BarChart
-                width={500}
+                width={550}
                 height={300}
                 data={dataCategoriasAgrupadas}
                 margin={{
@@ -133,7 +178,7 @@ function Dashboard() {
               >
                 <CartesianGrid strokeDasharray="5 5" />
                 <XAxis dataKey="nome" />
-                <YAxis />
+                <YAxis type="number" domain={[0, dataMaiorDebito]} />
                 <Tooltip />
                 <Bar dataKey="valor" fill="#8884d8">
                   {dataCategoriasAgrupadas.map((entry, index) => (
@@ -142,62 +187,6 @@ function Dashboard() {
                 </Bar>
               </BarChart>
             </div>
-            <div>
-              <div className="text-graph">
-                % Meus Investimentos
-              </div>
-              <BarChart
-                width={500}
-                height={300}
-                data={dataInvestimento}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5
-                }}
-              >
-                <CartesianGrid strokeDasharray="5 5" />
-                <XAxis dataKey="ativo" />
-                <YAxis />
-                <Tooltip />
-                <Bar dataKey="porcentagem" fill="#8884d8">
-                  {dataInvestimento.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                  ))}
-                </Bar>
-              </BarChart>
-            </div>
-          </div>
-          <div className="charts">
-            <div>
-              <div className="text-graph">
-                % investido por ativo
-              </div>
-              <PieChart width={500} height={350}>
-              <Tooltip />
-              <Legend />
-                <Pie data={dataInvestimento} label dataKey="porcentagem" nameKey="ativo" cx="50%" cy="50%" outerRadius={100} fill="#8884d8">
-                  {dataInvestimento.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                  ))}
-                </Pie>
-              </PieChart>
-            </div>
-            <div>   
-              <div className="text-graph">
-                Contas Pagas VS A Pagar
-              </div>
-              <PieChart width={500} height={300}>
-                <Legend />
-                <Tooltip />
-                <Pie data={dataContasPagoTotal} label dataKey="valor" nameKey="nome" cx="50%" cy="50%" outerRadius={100} fill="#8884d8">
-                  {dataContasPagoTotal.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                  ))}
-                </Pie>
-              </PieChart>
-            </div> 
             <div>
               <div className="text-graph">
                 % por Categoria
@@ -213,10 +202,52 @@ function Dashboard() {
               </PieChart>
             </div>
           </div>
-        </div>
+          <div className="charts">
+            <div>
+              <div className="text-graph">
+                % investido por ativo
+              </div>
+              <PieChart width={500} height={350}>
+                <Tooltip />
+                <Legend />
+                <Pie data={dataInvestimento} label dataKey="porcentagem" nameKey="ativo" cx="50%" cy="50%" outerRadius={100} fill="#8884d8">
+                  {dataInvestimento.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </div>
 
+            <div>
+              <div className="text-graph">
+                Valor Meus Investimentos
+              </div>
+              <BarChart
+                width={600}
+                height={300}
+                data={dataInvestimento}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5
+                }}
+              >
+                <CartesianGrid strokeDasharray="5 5" />
+                <XAxis dataKey="ativo" />
+                <YAxis type="number" domain={[0, dataMaiorInvestimento]} />
+                <Tooltip />
+                <Bar dataKey="valor" fill="#8884d8">
+                  {dataInvestimento.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                  ))}
+                </Bar>
+              </BarChart>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
+    </div>
   );
 
 }
